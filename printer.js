a1fefdc58c5213adceb3e41e4a30585bb3b118ed
Joshua Saucedo <joshuansu0897@gmail.com>
2019-10-02 09:27:22 -0700

    salu2

--- printer.js
+++ printer.js
@@ -1,19 +1,20 @@
 $(() => {
-  const contributorsList = $("#contributorsList");
+  const contributorsList = $('#contributorsList')
 
   const contributors = [
-    { name: "Didier Valdez", github: "https://github.com/xDidier901" }
-  ];
+    { name: 'Didier Valdez', github: 'https://github.com/xDidier901' },
+    { name: 'Joshua Saucedo', github: 'https://github.com/joshuansu0897'}
+  ]
 
   function printNames() {
     contributors.forEach(contributor => {
       contributorsList.append(`
       <div>
-        <p>${contributor.name} <a href="${contributor.github}">My GitHub</a></p>
-    </div>
-      `);
-    });
+        <a target='_blank' class='list-group-item list-group-item-action' href='${contributor.github}'>${contributor.name}</a>
+      </div>
+      `)
+    })
   }
 
-  printNames();
-});
+  printNames()
+})
